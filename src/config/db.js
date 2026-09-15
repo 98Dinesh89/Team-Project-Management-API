3a87495a3425cb327833508f04c117a770109fbb
98Dinesh89 <dinsun9983@gmail.com>
2026-09-15 20:34:51 +0530

    mongodb connection and models done

---
+++ src/config/db.js
@@ -0,0 +1,19 @@
+import mongoose from "mongoose";
+import { ENV } from "./env.js";
+
+const connectDB = async () => {
+    try {
+        const MONGODB_URI = ENV.MONGODB_URI;
+        if (!MONGODB_URI) throw new Error("MONGODB_URI not set");
+
+        const conn = await mongoose.connect(ENV.MONGODB_URI);
+        console.log("MONGODB CONNECTED: ", conn.connection.host);
+        console.log("----------------------------------------------------------------------------------------");
+
+    } catch (error) {
+        console.error("Error connecting to mongodb ", error);
+        process.exit(1);
+    }
+}
+
+export default connectDB;
--- src/config/env.js
+++ src/config/env.js
@@ -4,5 +4,6 @@ dotenv.config();
 
 export const ENV = {
     PORT: process.env.PORT,
-    NODE_ENV: process.env.NODE_ENV
+    NODE_ENV: process.env.NODE_ENV,
+    MONGODB_URI: process.env.MONGODB_URI
 }
