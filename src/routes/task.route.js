ba55d17233057cd849424528f85c2422d7012383
98Dinesh89 <dinsun9983@gmail.com>
2026-09-17 15:52:11 +0530

    get task endpoint added

--- src/routes/task.route.js
+++ src/routes/task.route.js
@@ -1,9 +1,10 @@
 import express from "express";
 import { protectRoute } from "../middlewares/auth.middleware.js";
-import { createTask } from "../contorollers/task.controller.js";
+import { createTask, getTasks } from "../contorollers/task.controller.js";
 
 const router = express.Router();
 
 router.post("/:projectId", protectRoute, createTask);
+router.get("/:projectId", protectRoute, getTasks);
 
 export default router;
